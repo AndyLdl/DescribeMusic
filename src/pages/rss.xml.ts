214626b2ef20a7f95b95473243eea2e4af17f55d
AndyLdl <ldliangcool@163.com>
2025-11-03 13:06:58 +0800

    refactor: Clean up formatting and indentation in RSS feed generation code

--- src/pages/rss.xml.ts
+++ src/pages/rss.xml.ts
@@ -2,30 +2,30 @@ import type { APIRoute } from 'astro';
 import { getCollection } from 'astro:content';
 
 export const GET: APIRoute = async ({ site }) => {
-  // 获取所有已发布的博客文章
-  const blogPosts = await getCollection('blog', ({ data }) => {
-    return !data.draft && data.publishDate < new Date();
-  });
+    // 获取所有已发布的博客文章
+    const blogPosts = await getCollection('blog', ({ data }) => {
+        return !data.draft && data.publishDate < new Date();
+    });
 
-  // 按发布日期排序（最新的在前）
-  blogPosts.sort((a, b) => {
-    return b.data.publishDate.valueOf() - a.data.publishDate.valueOf();
-  });
+    // 按发布日期排序（最新的在前）
+    blogPosts.sort((a, b) => {
+        return b.data.publishDate.valueOf() - a.data.publishDate.valueOf();
+    });
 
-  const siteUrl = site?.toString().replace(/\/$/, '') || 'https://describemusic.net';
-  const blogUrl = `${siteUrl}/blog`;
+    const siteUrl = site?.toString().replace(/\/$/, '') || 'https://describemusic.net';
+    const blogUrl = `${siteUrl}/blog`;
 
-  // 生成 RSS XML
-  const rssItems = blogPosts.map((post) => {
-    const postUrl = `${siteUrl}/blog/${post.slug}`;
-    const pubDate = post.data.publishDate.toUTCString();
-    const imageUrl = post.data.image?.src
-      ? post.data.image.src.startsWith('http')
-        ? post.data.image.src
-        : `${siteUrl}${post.data.image.src}`
-      : '';
+    // 生成 RSS XML
+    const rssItems = blogPosts.map((post) => {
+        const postUrl = `${siteUrl}/blog/${post.slug}`;
+        const pubDate = post.data.publishDate.toUTCString();
+        const imageUrl = post.data.image?.src
+            ? post.data.image.src.startsWith('http')
+                ? post.data.image.src
+                : `${siteUrl}${post.data.image.src}`
+            : '';
 
-    return `
+        return `
     <item>
       <title><![CDATA[${post.data.title}]]></title>
       <link>${postUrl}</link>
@@ -37,9 +37,9 @@ export const GET: APIRoute = async ({ site }) => {
       <category>${post.data.category}</category>
       ${post.data.tags.map((tag: string) => `<category>${tag}</category>`).join('\n      ')}
     </item>`;
-  }).join('\n');
+    }).join('\n');
 
-  const rss = `<?xml version="1.0" encoding="UTF-8"?>
+    const rss = `<?xml version="1.0" encoding="UTF-8"?>
 <rss version="2.0" xmlns:content="http://purl.org/rss/1.0/modules/content/" xmlns:wfw="http://wellformedweb.org/CommentAPI/" xmlns:dc="http://purl.org/dc/elements/1.1/" xmlns:atom="http://www.w3.org/2005/Atom" xmlns:sy="http://purl.org/rss/1.0/modules/syndication/" xmlns:slash="http://purl.org/rss/1.0/modules/slash/">
   <channel>
     <title>Describe Music Blog - AI Audio Analysis Insights & Tutorials</title>
@@ -53,11 +53,11 @@ export const GET: APIRoute = async ({ site }) => {
   </channel>
 </rss>`;
 
-  return new Response(rss, {
-    headers: {
-      'Content-Type': 'application/xml; charset=utf-8',
-      'Cache-Control': 'public, max-age=3600',
-    },
-  });
+    return new Response(rss, {
+        headers: {
+            'Content-Type': 'application/xml; charset=utf-8',
+            'Cache-Control': 'public, max-age=3600',
+        },
+    });
 };
 
